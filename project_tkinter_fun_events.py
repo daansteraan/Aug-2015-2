d311a7e7de53e721ba7927df2aab12f105f5b470
daansteraan <102012@CPTAF-D006629.africa.corp.sasfin.net>
2015-08-18 15:35:46 +0200

    180815-3

---
+++ project_tkinter_fun_events.py
@@ -0,0 +1,18 @@
+''' Some Tkinter event loops'''
+
+from Tkinter import *
+import Tkinter as tkinter
+
+root = Tk()
+root.title('event window')
+
+label_1 = tkinter.Label(root, text='waiting...', padx=200,pady=200)
+label_1.pack(fill='both')
+
+label_1.bind('<Enter>', lambda event: label_1.configure(text='pointer entered window'))
+label_1.bind('<Leave>', lambda event: label_1.configure(text='pointer left window'))
+label_1.bind('<1>', lambda event: label_1.configure(text='left clicked at %d,%d' % (event.x, event.y)))
+label_1.bind('<3>', lambda event: label_1.configure(text='right clicked at %d,%d' % (event.x, event.y)))
+
+
+root.mainloop()
