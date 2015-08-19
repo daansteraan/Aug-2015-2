d216a6ad145981549b306a52f70aabe723701f93
daansteraan <102012@CPTAF-D006629.africa.corp.sasfin.net>
2015-08-19 09:36:14 +0200

    190815-1

--- project_tkinter_fun_events.py
+++ project_tkinter_fun_events.py
@@ -6,9 +6,11 @@ import Tkinter as tkinter
 root = Tk()
 root.title('event window')
 
-label_1 = tkinter.Label(root, text='waiting...', padx=200,pady=200)
-label_1.pack(fill='both')
+frame = tkinter.Frame(root, relief='raised').grid(row=0,column=0,sticky=(N,S,E,W))
 
+label_1 = tkinter.Label(frame, text='waiting...', padx=200,pady=200)
+
+label_1.pack(fill='both')
 label_1.bind('<Enter>', lambda event: label_1.configure(text='pointer entered window'))
 label_1.bind('<Leave>', lambda event: label_1.configure(text='pointer left window'))
 label_1.bind('<1>', lambda event: label_1.configure(text='left clicked at %d,%d' % (event.x, event.y)))
