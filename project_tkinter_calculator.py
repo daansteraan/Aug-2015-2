5b7e99ea574f0a3b96fa94ff9af1f24ad178f87a
daansteraan <102012@CPTAF-D006629.africa.corp.sasfin.net>
2015-08-20 16:59:07 +0200

    200815-1

--- project_tkinter_calculator.py
+++ project_tkinter_calculator.py
@@ -1,30 +1,40 @@
 from Tkinter import *
 import Tkinter as tk
 
+# main window
 root = Tk()
+root.title('Calculator')
 
-buttons = ['1','2','3','4','5','6','7','8','9','0','+','-','/','*','=']
-row = 1
-column = 1
+# button set
+buttons = ['1','2','3','4','5','6','7','8','9','0','+','-','/','*','.']
 
-frame = tk.Frame(root)
-frame.grid(row=0, column=0, sticky=(N,S,E,W))
-frame.rowconfigure(0,weight=1)
-frame.columnconfigure(0,weight=1)
+# calculator sum value
+sum_value = StringVar()
+input_value = StringVar()        
 
+def calc(x):   
+    sum_value.set(x)
+    return sum_value
+    
+# output window
+output_window = tk.Label(root, textvariable=sum_value, width=20, height=2).grid(row=0, columnspan=3, sticky=(E,W))
 
-calc_window = StringVar()
-window = tk.Label(root, textvariable=calc_window)
-window.pack(side='top')
+# button creation
+r=1
+c=0
 
 for i in buttons:
-    if row < 3:
-        tk.Button(frame,text=i).grid(row=row,column=column)
-        row += 1
-        column += 1
+    if c < 2:
+        tk.Button(root, text = i, command = calc(i), padx = 5, pady = 3).grid(row = r, column = c, sticky = (N,S,E,W))        
+        c += 1
     else:
-        row = 1
-        column = 1
+        tk.Button(root, text = i, command = calc(i), padx = 5, pady = 3).grid(row = r,column = c,sticky = (N,S,E,W))
+        r  += 1
+        c = 0
         
+# clear and equals button
+clear = tk.Button(root,text='=',padx = 5, pady=3).grid(row=6,column=0,sticky=(N,S,E,W))
+clear = tk.Button(root,text='CLEAR',padx = 5, pady=3).grid(row=6,column=1, columnspan = 2,sticky=(N,S,E,W))
+
 root.mainloop()
     
