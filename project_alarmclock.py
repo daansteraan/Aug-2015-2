442e3bd868b8602baefda2d8f3382f8b80ef85bd
daansteraan <102012@CPTAF-D006629.africa.corp.sasfin.net>
2015-08-27 17:06:19 +0200

    270815-2
    Alarm Clock Desktop App

--- project_alarmclock.py
+++ project_alarmclock.py
@@ -3,38 +3,45 @@
 from Tkinter import *
 import Tkinter as tk
 import time
+import winsound
 
 alarm_clock = Tk()
 alarm_clock.title('Alarm Clock')
-alarm_clock.maxsize(height=110)
+alarm_clock.iconbitmap('favicon.ico')
+alarm_clock.resizable(width=FALSE,height=FALSE)
 
 current_time = ''
 
 timer = tk.Label(alarm_clock,text=current_time)
 timer.grid(row=0,column=1,pady=5, padx=20,columnspan=3)
-time_label = tk.Label(alarm_clock, text = 'TIME:').grid(row=0,column=0)
-
-        
+time_label = tk.Label(alarm_clock, text = 'TIME :').grid(row=0,column=0)        
 
 def updater():
     new_time = time.strftime("%H    |      %M     |     %S")
     timer.config(text=new_time)
     alarm_clock.after(100,updater)
     
-alarm_time = tk.Label(alarm_clock, text = 'Set Alarm: ').grid(row=2,column=0, padx= 5,pady=5)
-hour_label = tk.Label(alarm_clock, text = 'H').grid(row=1,column=1,)
-minute_label = tk.Label(alarm_clock, text = 'M').grid(row=1,column=2)
-second_label = tk.Label(alarm_clock, text = 'S').grid(row=1,column=3)
+alarm_time = tk.Label(alarm_clock, text = 'Alarm Time : ').grid(row=2,column=0, padx= 5,pady=5)
 
-def test():
-    alarm_time = '%s:%s:%s' %(hours,minutes,seconds)        
+def set_alarm():     
+    global alarm_check
     time_now = time.strftime('%H:%M%S')
+    test.config(text='ACTIVATED')   
     
-    print "Alarm time = ",alarm_time
-    print "Time now= ", time_now    
+    h = int(time_now[0:2])    
+    m = int(time_now[3:5])    
+    s = int(time_now[5:])
     
-def clear():
+    if (h==hour) and (m==minute) and (s==second):
+        print 'SOUND THE ALARM'
+        for i in range(20):
+            winsound.Beep(1000,500)
+    
+    alarm_check = alarm_clock.after(10,set_alarm)
+    
+def reset():
     global hour, minute, second
+    
     hour = 0
     minute = 0
     second = 0
@@ -43,8 +50,18 @@ def clear():
     minutes.config(text='0'+str(minute%60))
     seconds.config(text='0'+str(second%60))
     
-test = tk.Button(alarm_clock, text='TEST', command=test).grid(row=3,column=0,sticky=(E,W))
-clear = tk.Button(alarm_clock, text='RESET', command=clear).grid(row=4,column=0,sticky=(E,W))
+    test.config(text='SET ALARM')    
+    
+    try:
+        alarm_clock.after_cancel(alarm_check)
+    except:
+        pass
+    
+test = tk.Button(alarm_clock, text='SET ALARM', command=set_alarm)
+test.grid(row=3,column=1,columnspan=3,sticky=(E,W))
+clear = tk.Button(alarm_clock, text='RESET', command=reset)
+clear.grid(row=3,column=0,sticky=(E,W))
+
         
 # alarm functions
 hour = 0
@@ -83,5 +100,6 @@ minutes.grid(row=2,column=2,sticky=(E,W))
 seconds = tk.Button(alarm_clock, text=second, command = second_increment)
 seconds.grid(row=2,column=3,sticky=(E,W))
 
+reset()
 updater() 
 alarm_clock.mainloop()
